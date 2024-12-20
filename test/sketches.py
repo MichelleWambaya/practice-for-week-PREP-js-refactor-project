58f3c347fedafc152a89f2ed4c1d3bee49ee6f3c
Michelle Wambaya <michellewambaya@gmail.com>
2024-12-20 06:49:03 +0300

    initial commit

---
+++ test/sketches.py
@@ -0,0 +1,11 @@
+def fizzBuzz(n):
+    for(let i = 0; i<n; i++){
+      if i % 3 === 0 && i%5 === 0:
+          print("FizzBuzz")
+          elif i%3 === 0 && i%5 !==0:
+              print("Fizz")
+              else:
+                  print("Buzz")
+    }
+    print("i")
+    fizzBuzz(15)
